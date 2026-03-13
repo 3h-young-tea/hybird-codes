9a7fff0dc7cde1eb1460ae374b0024e93af17b35
young_tea <3h.young.tea@gmail.com>
2026-03-13 14:40:18 +0800

    Refactor tree class to use smart pointers

--- range_kth.cpp
+++ range_kth.cpp
@@ -1,83 +1,106 @@
-#include<bits/stdc++.h>
-using namespace std::string_literals;
-#define try "i`ve aked wf!"s
-using lf=double;
-using ll=std::ptrdiff_t;
-using ull=std::size_t;
-using us=unsigned;
-#define uoj unique_ptr
-#define qoj shared_ptr
-#define muoj make_unique
-#define mqoj make_shared
-constexpr ll inf=0x3f3f3f3f3f3f3f3fl;
-constexpr ll mod=0x3b800001l;
-auto nxt(void){ll x;std::cin>>x;return x;}
+#include <bits/stdc++.h>
+
+using std::complex_literals::operator""i;
+using std::string_literals::operator""s;
+
+using lf = double;
+using ll = std::ptrdiff_t;
+using ull = std::size_t;
+using us = unsigned;
+
+constexpr ll k_inf = 0x3f3f3f3f3f3f3f3fl;
+constexpr ll k_mod = 0x3b800001l;
+
+auto	nxt(void) {
+	ll x;
+	std::cin >> x;
+	return x;
+}
+
 /*
 prob link:	https://vjudge.net/problem/%E6%B4%9B%E8%B0%B7-P3834
+		https://vjudge.net/problem/%E6%B4%9B%E8%B0%B7-T741150
 start think:	
 term think:	
-start code:	mar 6th 2026, 12:39
-term code:	mar 6th 2026, 12:52
-start dbg:	mar 6th 2026, 12:54
-term dbg:	mar 6th 2026, 13:06
-start dbg:	mar 6th 2026, 13:27
-term dbg:	mar 6th 2026, 13:36
+start code:	
+term code:	
 */
-class	tree{
-	std::qoj<tree>ld,rd;
-	ll f,g,tot;
+
+class	tree {
+	std::shared_ptr<tree> ld_, rd_;
+	ll f_, g_, tot_;
+
 public:
-	tree(ll,ll);
-	void build(ll,ll);
-	void update(std::qoj<tree>&,ll,ll);
-	auto kth(std::qoj<tree>&,ll);
+	tree(ll, ll);
+	void update(std::shared_ptr<tree>&, ll, ll);
+	auto kth(std::shared_ptr<tree>&, ll) const;
 };
-tree::tree(ll p,ll q){
-	f=p,g=q,tot=0;
-}
-void	tree::update(std::qoj<tree>&x,ll p,ll k){
-	if(x){
-		ld=x->ld,rd=x->rd;
-		tot=x->tot+k;
-	}else	tot=k;
-	if(f==g)
+
+tree::tree(ll p, ll q)
+	: f_(p), g_(q), tot_(0) {}
+
+void	tree::update(std::shared_ptr<tree> &y, ll p, ll k) {
+	if (y) {
+		ld_ = y->ld_;
+		rd_ = y->rd_;
+		tot_ = y->tot_ + k;
+	} else
+		tot_ = k;
+
+	if (f_ == g_)
 		return;
-	if(p<=f+g>>1)
-		ld=std::mqoj<tree>(f,f+g>>1),
-		ld->update(x?x->ld:x,p,k);
-	else	rd=std::mqoj<tree>((f+g>>1)+1,g),
-		rd->update(x?x->rd:x,p,k);
-}
-auto	tree::kth(std::qoj<tree>&x,ll k){
-	if(f==g)
-		return f;
-	ll ldt=(ld?ld->tot:0)-(x&&x->ld?x->ld->tot:0);
-	if(k<=ldt)
-		return ld->kth(x?x->ld:x,k);
-	else	return rd->kth(x?x->rd:x,k-ldt);
-}
-void	solve(void){
-	ll n=nxt(),q=nxt(),v=(1l<<30)-1;
-	auto rt=std::muoj<std::qoj<tree>[]>(n+1);
-	rt[0]=std::mqoj<tree>(0,v);
-	for(ll x=1;x<=n;x++)
-		rt[x]=std::mqoj<tree>(0,v),
-		rt[x]->update(rt[x-1],nxt(),1);
-	while(q--){
-		ll x=nxt(),y=nxt(),k=nxt();
-		std::cout<<rt[y]->kth(rt[x-1],k)<<"\n"s;
+
+	if (p <= f_ + g_ >> 1) {
+		ld_ = std::make_shared<tree>(f_, f_ + g_ >> 1);
+		ld_->update(y ? y->ld_ : y, p, k);
+	} else {
+		rd_ = std::make_shared<tree>((f_ + g_ >> 1) + 1, g_);
+		rd_->update(y ? y->rd_ : y, p, k);
 	}
 }
-auto	main(void)->signed{
-//	std::freopen(".in","r",stdin);
-//	std::freopen(".out","w",stdout);
+
+auto	tree::kth(std::shared_ptr<tree> &y, ll k) const {
+	if (f_ == g_)
+		return f_;
+
+	ll ld_t = (ld_ ? ld_->tot_ : 0) - (y && y->ld_ ? y->ld_->tot_ : 0);
+
+	if (k <= ld_t)
+		return ld_->kth(y ? y->ld_ : y, k);
+	else
+		return rd_->kth(y ? y->rd_ : y, k - ld_t);
+}
+
+void	solve(void) {
+	ll len = nxt(), queries = nxt(), range = (1l << 30) - 1;
+	auto rt = std::make_unique<std::shared_ptr<tree>[]>(len + 1);
+	rt[0] = std::make_shared<tree>(0, range);
+
+	for (ll x = 1; x <= len; ++x){
+		rt[x] = std::make_shared<tree>(0, range);
+		rt[x]->update(rt[x - 1], nxt(), 1);
+	}
+
+	while (queries--) {
+		ll x = nxt(), y = nxt(), k = nxt();
+		std::cout << rt[y]->kth(rt[x - 1], k) << "\n"s;
+	}
+}
+
+auto	main(void)->signed {
+//	std::freopen(".in", "r", stdin);
+//	std::freopen(".out", "w", stdout);
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
-	ll _=1;
-//	_=nxt();
-	while(_--)
+
+	ll _ = 1;
+
+//	_ = nxt();
+
+	while (_--)
 		solve();
+
 	std::cout.flush();
 	return 0;
 }
