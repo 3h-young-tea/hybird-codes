98064139062bc213e22ee928265ab52fc9f4bf83
young_tea <3h.young.tea@gmail.com>
2026-03-25 10:32:33 +0800

    Change data types from ll to long for consistency

--- range_kth.cpp
+++ range_kth.cpp
@@ -3,23 +3,17 @@
 using std::complex_literals::operator""i;
 using std::string_literals::operator""s;
 
-using lf = double;
-using ll = std::ptrdiff_t;
-using ull = std::size_t;
-using us = unsigned;
-
-constexpr ll k_inf = 0x3f3f3f3f3f3f3f3fl;
-constexpr ll k_mod = 0x3b800001l;
+constexpr long k_inf = 0x3f3f3f3f3f3f3f3fl;
+constexpr long k_mod = 0x3b800001l;
 
 auto	nxt(void) {
-	ll x;
+	long x;
 	std::cin >> x;
 	return x;
 }
 
 /*
 prob link:	https://vjudge.net/problem/%E6%B4%9B%E8%B0%B7-P3834
-		https://vjudge.net/problem/%E6%B4%9B%E8%B0%B7-T741150
 start think:	
 term think:	
 start code:	
@@ -28,73 +22,86 @@ term code:
 
 class	tree {
 	std::shared_ptr<tree> ld_, rd_;
-	ll f_, g_, tot_;
+	long f_, g_, tot_;
 
 public:
-	tree(ll, ll);
-	void update(std::shared_ptr<tree>&, ll, ll);
-	auto kth(std::shared_ptr<tree>&, ll) const;
+	tree(long, long);
+	tree(const std::shared_ptr<tree>&, long, long);
+	long kth(const std::shared_ptr<tree>&, long) const;
 };
 
-tree::tree(ll p, ll q)
-	: f_(p), g_(q), tot_(0) {}
-
-void	tree::update(std::shared_ptr<tree> &y, ll p, ll k) {
-	if (y) {
-		ld_ = y->ld_;
-		rd_ = y->rd_;
-		tot_ = y->tot_ + k;
-	} else
-		tot_ = k;
-
+tree::tree(long f, long g)
+	: f_(f), g_(g), tot_(0) {
 	if (f_ == g_)
 		return;
 
-	if (p <= f_ + g_ >> 1) {
-		ld_ = std::make_shared<tree>(f_, f_ + g_ >> 1);
-		ld_->update(y ? y->ld_ : y, p, k);
-	} else {
-		rd_ = std::make_shared<tree>((f_ + g_ >> 1) + 1, g_);
-		rd_->update(y ? y->rd_ : y, p, k);
-	}
+	ld_ = std::make_shared<tree>(f_, f_ + g_ >> 1);
+	rd_ = std::make_shared<tree>((f_ + g_ >> 1) + 1, g_);
 }
 
-auto	tree::kth(std::shared_ptr<tree> &y, ll k) const {
+tree::tree(const std::shared_ptr<tree> &y, long p, long k)
+	: ld_(y->ld_), rd_(y->rd_),
+	f_(y->f_), g_(y->g_), tot_(y->tot_ + k) {
+	if (f_ == g_)
+		return;
+
+	if (p <= f_ + g_ >> 1)
+		ld_ = std::make_shared<tree>(y->ld_, p, k);
+	else
+		rd_ = std::make_shared<tree>(y->rd_, p, k);
+}
+
+long	tree::kth(const std::shared_ptr<tree> &y, long k) const {
 	if (f_ == g_)
 		return f_;
 
-	ll ld_t = (ld_ ? ld_->tot_ : 0) - (y && y->ld_ ? y->ld_->tot_ : 0);
+	long ld_tot = ld_->tot_ - y->ld_->tot_;
 
-	if (k <= ld_t)
-		return ld_->kth(y ? y->ld_ : y, k);
+	if (k <= ld_tot)
+		return ld_->kth(y->ld_, k);
 	else
-		return rd_->kth(y ? y->rd_ : y, k - ld_t);
+		return rd_->kth(y->rd_, k - ld_tot);
+}
+
+long	val_to_rank(std::vector<long> &a, std::vector<long> &b) {
+	std::copy(a.begin(), a.end(), b.begin());
+	std::sort(b.begin(), b.end());
+	b.erase(std::unique(b.begin(), b.end()), b.end());
+
+	for (long &x : a)
+		x = std::lower_bound(b.begin(), b.end(), x) - b.begin() + 1;
+
+	return b.size();
 }
 
 void	solve(void) {
-	ll len = nxt(), queries = nxt(), range = (1l << 30) - 1;
-	auto rt = std::make_unique<std::shared_ptr<tree>[]>(len + 1);
-	rt[0] = std::make_shared<tree>(0, range);
+	long len = nxt(), queries = nxt();
+	std::vector<long> a(len), b(len);
+	std::generate(a.begin(), a.end(), nxt);
+	long v = val_to_rank(a, b);
 
-	for (ll x = 1; x <= len; ++x){
-		rt[x] = std::make_shared<tree>(0, range);
-		rt[x]->update(rt[x - 1], nxt(), 1);
-	}
+	std::vector<std::shared_ptr<tree>> rt;
+	rt.emplace_back(std::make_shared<tree>(1, v));
+
+	for (long x : a)
+		rt.emplace_back(std::make_shared<tree>(rt[rt.size() - 1], x, 1));
 
 	while (queries--) {
-		ll x = nxt(), y = nxt(), k = nxt();
-		std::cout << rt[y]->kth(rt[x - 1], k) << "\n"s;
+		long x = nxt(), y = nxt(), k = nxt();
+		std::cout << b[rt[y]->kth(rt[x - 1], k) - 1] << "\n"s;
 	}
+
+	std::exit(0);
 }
 
-auto	main(void)->signed {
+signed	main(void) {
 //	std::freopen(".in", "r", stdin);
 //	std::freopen(".out", "w", stdout);
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
 
-	ll _ = 1;
+	long _ = 1;
 
 //	_ = nxt();
 
@@ -102,5 +109,5 @@ auto	main(void)->signed {
 		solve();
 
 	std::cout.flush();
-	return 0;
+	return EXIT_SUCCESS;
 }
